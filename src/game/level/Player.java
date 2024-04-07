8bc6908b866555994feb52f703423cfae382e974
Jan Koubek <koubek.jenda@icloud.com>
2024-04-07 19:24:01 +0200

    added constructor

--- src/game/level/Player.java
+++ src/game/level/Player.java
@@ -1,10 +1,46 @@
 package game.level;
 
+import renderer.Animation;
 import renderer.Sprite;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Player {
+
+    private static final int PLAYER_WIDTH = 16;
+    private static final int PLAYER_HEIGHT = 16;
+    private static final String PLAYER_TEXTURES_DIRECTORY = "assets/textures/player/";
+
     private Rectangle position;
     private Sprite sprite;
+
+    public Player() {
+        position = new Rectangle(0, 0, 16, 16);
+        HashMap<String, Animation> animations = new HashMap<>();
+        ArrayList<BufferedImage> images = new ArrayList<>();
+
+        try {
+            images.add(ImageIO.read(new File(PLAYER_TEXTURES_DIRECTORY + "running/run1.png")));
+            images.add(ImageIO.read(new File(PLAYER_TEXTURES_DIRECTORY + "running/run2.png")));
+            images.add(ImageIO.read(new File(PLAYER_TEXTURES_DIRECTORY + "running/run.png")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        animations.put("running", new Animation(images, (1/60) * 1000, true));
+
+        try {
+            animations.put("static", new Animation(ImageIO.read(new File(PLAYER_TEXTURES_DIRECTORY + "static.png"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        sprite = new Sprite(animations, PLAYER_WIDTH, PLAYER_HEIGHT);
+    }
 }
