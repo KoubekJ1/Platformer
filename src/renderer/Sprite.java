a866085d5a870f1305c173bae3982e17a9b2000c
Jan Koubek <koubek.jenda@icloud.com>
2024-04-07 15:37:33 +0200

    modified fields

--- src/renderer/Sprite.java
+++ src/renderer/Sprite.java
@@ -5,8 +5,16 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class Sprite {
-    private BufferedImage activeImage;
     private ArrayList<BufferedImage> images;
+    private int activeImage;
     private Timer animationTimer;
     private boolean loop;
+
+    public Sprite(BufferedImage image) {
+        images = new ArrayList<>();
+        images.add(image);
+        activeImage = 0;
+    }
+
+
 }
