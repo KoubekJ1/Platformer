538cc06a9a17afb753b2407c4a992e0094a33791
Jan Koubek <koubek.jenda@icloud.com>
2024-04-07 13:56:32 +0200

    removed redundant fields

--- src/renderer/gameplay/GameplayJPanel.java
+++ src/renderer/gameplay/GameplayJPanel.java
@@ -3,10 +3,6 @@ package renderer.gameplay;
 import javax.swing.*;
 
 public class GameplayJPanel extends JPanel {
-
-    private JPanel gameplayPanel;
-    private JLabel scoreLabel;
-
     public void initialize() {
 
     }
