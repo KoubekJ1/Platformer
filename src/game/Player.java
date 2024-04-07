aff10eb50d6a9bb1c8c96d57d6e90508dd786308
Jan Koubek <koubek.jenda@icloud.com>
2024-04-07 17:30:33 +0200

    added Player fields

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,4 +1,12 @@
 package game;
 
+import renderer.Sprite;
+
+import java.awt.*;
+
 public class Player {
+    private Point position;
+    private int width;
+    private int height;
+    private Sprite sprite;
 }
