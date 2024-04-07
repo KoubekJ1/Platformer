037fcae88cd7999ca13039321b47af09eceeafa0
Jan Koubek <koubek.jenda@icloud.com>
2024-04-07 18:52:48 +0200

    implemented ActionListener

--- src/renderer/Animation.java
+++ src/renderer/Animation.java
@@ -1,13 +1,21 @@
 package renderer;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.util.HashSet;
 import java.util.Timer;
 
-public class Animation {
+public class Animation implements ActionListener {
     private HashSet<BufferedImage> images;
     private int[] imageOrder;
     private boolean loop;
     private int currentImage;
     private Timer animationTimer;
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
