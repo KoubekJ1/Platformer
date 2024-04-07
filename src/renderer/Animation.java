c9ddc131418407bff72d0fe27b380fe160d401ae
Jan Koubek <koubek.jenda@icloud.com>
2024-04-07 18:52:19 +0200

    added fields

--- src/renderer/Animation.java
+++ src/renderer/Animation.java
@@ -1,4 +1,13 @@
 package renderer;
 
+import java.awt.image.BufferedImage;
+import java.util.HashSet;
+import java.util.Timer;
+
 public class Animation {
+    private HashSet<BufferedImage> images;
+    private int[] imageOrder;
+    private boolean loop;
+    private int currentImage;
+    private Timer animationTimer;
 }
