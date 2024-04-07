b25b7928ec418626d3df853d305a7bd011b2aace
Jan Koubek <koubek.jenda@icloud.com>
2024-04-07 18:56:19 +0200

    added constructor, changed image HashSet to ArrayList

--- src/renderer/Animation.java
+++ src/renderer/Animation.java
@@ -1,18 +1,24 @@
 package renderer;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.util.HashSet;
-import java.util.Timer;
+import java.util.ArrayList;
 
 public class Animation implements ActionListener {
-    private HashSet<BufferedImage> images;
+    private ArrayList<BufferedImage> images;
     private int[] imageOrder;
     private boolean loop;
     private int currentImage;
     private Timer animationTimer;
 
+    public Animation(BufferedImage image) {
+        images = new ArrayList<>();
+        images.add(image);
+        loop = false;
+        currentImage = 0;
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
