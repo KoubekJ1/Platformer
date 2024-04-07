f18aa5d287fb8acf3ced20b2391754dac70a3d14
Jan Koubek <koubek.jenda@icloud.com>
2024-04-07 17:32:06 +0200

    added Level fields

--- src/game/level/Level.java
+++ src/game/level/Level.java
@@ -1,4 +1,12 @@
 package game.level;
 
+import game.Enemy;
+import game.Player;
+
+import java.util.LinkedList;
+
 public class Level {
+    private Player player;
+    private Block[][] blocks;
+    private LinkedList<Enemy> enemies;
 }
