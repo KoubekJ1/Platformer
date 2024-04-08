f3b784440c35417ce14f3a744fbb4cf099b6d5ac
Jan Koubek <koubek.jenda@icloud.com>
2024-04-08 19:47:15 +0200

    added baseBlockSize variable and a method for updating it

--- src/renderer/Renderer.java
+++ src/renderer/Renderer.java
@@ -1,13 +1,21 @@
 package renderer;
 
+import renderer.window.WindowManager;
+
 import java.awt.*;
 
 public abstract class Renderer {
 
     private static Graphics2D g2D;
+    private static int baseBlockSize;
 
     public static void setGraphics(Graphics2D graphics) {
         Renderer.g2D = graphics;
+        updateBaseBlockSize();
+    }
+
+    private static void updateBaseBlockSize() {
+        baseBlockSize = WindowManager.getResolution()[0];
     }
 
 }
