31151fec81ac1d84cef9d16a973b641ba1bf17ab
Jan Koubek <koubek.jenda@icloud.com>
2024-04-08 20:20:27 +0200

    added camera to player

--- src/game/level/Player.java
+++ src/game/level/Player.java
@@ -20,9 +20,11 @@ public class Player {
 
     private Rectangle position;
     private Sprite sprite;
+    private Camera camera;
 
     public Player() {
         position = new Rectangle(0, 0, 1, 1);
+        camera = new Camera();
         HashMap<String, Animation> animations = new HashMap<>();
         ArrayList<BufferedImage> images = new ArrayList<>();
 
