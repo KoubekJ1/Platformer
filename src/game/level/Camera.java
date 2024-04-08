963afaa18b4f6a292737d7ca1c06fc8c37576093
Jan Koubek <koubek.jenda@icloud.com>
2024-04-08 20:16:33 +0200

    changed camera to work with floating point values to increase accuracy

--- src/game/level/Camera.java
+++ src/game/level/Camera.java
@@ -10,17 +10,13 @@ public class Camera {
         transform = new AffineTransform(1, 0, 0, 1, 0, 0); // Base scale, upper-left corner position
     }
 
-    public void setPosition(int x, int y) {
+    public void setPosition(double x, double y) {
         transform.translate(x, y);
-    }
-
-    public void setPosition(Point point) {
-        setPosition(point.x, point.y);
     }
 
     public void setWorldScale(int scale) {
         // Weird workaround, the scale() method doesn't save translation, translation is applied again after scaling
-        int[] translation = new int[]{(int) transform.getTranslateX(), (int) transform.getTranslateY()};
+        double[] translation = new double[]{transform.getTranslateX(), transform.getTranslateY()};
         transform.scale(scale, scale);
         setPosition(translation[0], translation[1]);
     }
