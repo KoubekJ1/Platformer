43872b2d75b0cf859fa24837cecde810a2414a1c
Jan Koubek <koubek.jenda@icloud.com>
2024-04-08 20:07:49 +0200

    added methods for setting the camera's position and world scale

--- src/game/level/Camera.java
+++ src/game/level/Camera.java
@@ -9,4 +9,19 @@ public class Camera {
     public Camera() {
         transform = new AffineTransform(1, 0, 0, 1, 0, 0); // Base scale, upper-left corner position
     }
+
+    public void setPosition(int x, int y) {
+        transform.translate(x, y);
+    }
+
+    public void setPosition(Point point) {
+        setPosition(point.x, point.y);
+    }
+
+    public void setWorldScale(int scale) {
+        // Weird workaround, the scale() method doesn't save translation, translation is applied again after scaling
+        int[] translation = new int[]{(int) transform.getTranslateX(), (int) transform.getTranslateY()};
+        transform.scale(scale, scale);
+        setPosition(translation[0], translation[1]);
+    }
 }
