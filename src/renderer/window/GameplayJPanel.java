aae2d215d8ab210d96a2e344a728d97210b2da87
Jan Koubek <koubek.jenda@icloud.com>
2024-04-08 19:46:06 +0200

    set the panel to upload its graphics to the Renderer class

--- src/renderer/window/GameplayJPanel.java
+++ src/renderer/window/GameplayJPanel.java
@@ -1,9 +1,12 @@
 package renderer.window;
 
+import renderer.Renderer;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class GameplayJPanel extends JPanel {
     public void initialize() {
-
+        Renderer.setGraphics((Graphics2D) this.getGraphics());
     }
 }
