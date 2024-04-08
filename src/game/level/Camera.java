6edfdc9e9ded61ae88611219526dcdd269c3531d
Jan Koubek <koubek.jenda@icloud.com>
2024-04-08 19:57:18 +0200

    added Camera constructor

--- src/game/level/Camera.java
+++ src/game/level/Camera.java
@@ -5,4 +5,8 @@ import java.awt.geom.AffineTransform;
 
 public class Camera {
     private AffineTransform transform;
+
+    public Camera() {
+        transform = new AffineTransform(1, 0, 0, 1, 0, 0); // Base scale, upper-left corner position
+    }
 }
