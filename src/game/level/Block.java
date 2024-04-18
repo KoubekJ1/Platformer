a147d8251ceac77b05151078e0cdc7c3bfb3f8d3
Jan Koubek <koubek.jenda@icloud.com>
2024-04-18 17:21:41 +0200

    added getCurrentImage() method

--- src/game/level/Block.java
+++ src/game/level/Block.java
@@ -2,7 +2,13 @@ package game.level;
 
 import renderer.Sprite;
 
+import java.awt.image.BufferedImage;
+
 public class Block {
     private Sprite sprite;
     private boolean collision;
+
+    public BufferedImage getCurrentImage(float currentBlockSize) {
+        return sprite.getCurrentImage(currentBlockSize);
+    }
 }
