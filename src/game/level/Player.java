11722c5888d36c2b1b17d989e8b3721a8996a2a9
Jan Koubek <koubek.jenda@icloud.com>
2024-04-18 16:42:27 +0200

    modified Player class to utilize AssetManager

--- src/game/level/Player.java
+++ src/game/level/Player.java
@@ -2,6 +2,7 @@ package game.level;
 
 import renderer.Animation;
 import renderer.Sprite;
+import util.AssetManager;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -16,7 +17,7 @@ public class Player {
     private static final int PLAYER_WIDTH = 1;
     private static final int SMALL_PLAYER_HEIGHT = 1;
     private static final int LARGE_PLAYER_HEIGHT = 1;
-    private static final String PLAYER_TEXTURES_DIRECTORY = "assets/textures/player/";
+    private static final String PLAYER_TEXTURES_DIRECTORY = "player/";
 
     private Rectangle position;
     private Sprite sprite;
@@ -28,13 +29,9 @@ public class Player {
         HashMap<String, Animation> animations = new HashMap<>();
         ArrayList<BufferedImage> images = new ArrayList<>();
 
-        try {
-            images.add(ImageIO.read(new File(PLAYER_TEXTURES_DIRECTORY + "running/run1.png")));
-            images.add(ImageIO.read(new File(PLAYER_TEXTURES_DIRECTORY + "running/run2.png")));
-            images.add(ImageIO.read(new File(PLAYER_TEXTURES_DIRECTORY + "running/run3.png")));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        images.add(AssetManager.getTexture(PLAYER_TEXTURES_DIRECTORY + "run1.png"));
+        images.add(AssetManager.getTexture(PLAYER_TEXTURES_DIRECTORY + "run2.png"));
+        images.add(AssetManager.getTexture(PLAYER_TEXTURES_DIRECTORY + "run3.png"));
 
         animations.put("running", new Animation(images, (1/60) * 1000, true));
 
