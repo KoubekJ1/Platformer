6d041fdca5956195dfdf698a424dd625a3c31e0c
Jan Koubek <koubek.jenda@icloud.com>
2024-04-18 18:21:59 +0200

    implemented method for serialization

--- src/game/level/Level.java
+++ src/game/level/Level.java
@@ -2,10 +2,28 @@ package game.level;
 
 import game.level.enemy.Enemy;
 
+import java.io.*;
 import java.util.LinkedList;
 
-public class Level {
+public class Level implements Serializable {
+    private static final String LEVELS_DIRECTORY = "assets/levels/";
+
+    private String levelID;
+    private String levelName;
+
     private Player player;
     private Block[][] blocks;
     private LinkedList<Enemy> enemies;
+
+    public void serialize() throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(LEVELS_DIRECTORY + "custom/" + levelID + ".level");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(this);
+
+        objectOutputStream.flush();
+        objectOutputStream.close();
+        fileOutputStream.flush();
+        fileOutputStream.close();
+    }
+
 }
