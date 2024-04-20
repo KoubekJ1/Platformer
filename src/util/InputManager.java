f22356db4a4243415effd5dacf725f19aba348f4
Jan Koubek <koubek.jenda@icloud.com>
2024-04-20 14:32:44 +0200

    implemented InputManager functionality

--- src/util/InputManager.java
+++ src/util/InputManager.java
@@ -1,4 +1,19 @@
 package util;
 
-public class InputManager {
+import java.util.HashMap;
+
+public abstract class InputManager {
+    private static HashMap<Integer, Boolean> keyboardButtonPressed = new HashMap<>();
+
+    public void pressButton(int button) {
+        keyboardButtonPressed.put(button, true);
+    }
+
+    public void releaseButton(int button) {
+        keyboardButtonPressed.put(button, false);
+    }
+
+    public boolean getButtonPressed(int button) {
+        return keyboardButtonPressed.getOrDefault(button, false);
+    }
 }
