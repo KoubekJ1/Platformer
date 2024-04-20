cc35384d214c3c95c247a23157853c95937e0030
Jan Koubek <koubek.jenda@icloud.com>
2024-04-20 16:16:15 +0200

    added more debug info

--- src/renderer/window/GameplayJPanel.java
+++ src/renderer/window/GameplayJPanel.java
@@ -77,7 +77,11 @@ public class GameplayJPanel extends JPanel {
             g2D.setTransform(defaultTransform);
             g2D.setFont(new Font("Segoe UI", Font.PLAIN, 20));
             g2D.setColor(Color.BLACK);
-            g2D.drawString("FPS: " + renderInfo.getFrameRate(), 100, 100);
+            g2D.drawString("FPS: " + renderInfo.getFrameRate(), 0, 20);
+            float[] position = renderInfo.getPlayers().getFirst().getPosition();
+            g2D.drawString("Player position: " + position[0] + ", " + position[1], 0, 40);
+            float[] velocity = renderInfo.getPlayers().getFirst().getVelocity();
+            g2D.drawString("Player velocity: " + velocity[0] + ", " + velocity[1], 0, 60);
         }
     }
 
