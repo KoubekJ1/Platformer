4971c9b7ef5e40d4e850e01ac19d5a6f15ab4532
Jan Koubek <koubek.jenda@icloud.com>
2024-04-20 16:54:29 +0200

    renamed methods

--- src/util/InputManager.java
+++ src/util/InputManager.java
@@ -5,15 +5,15 @@ import java.util.HashMap;
 public abstract class InputManager {
     private static HashMap<Integer, Boolean> keyboardButtonPressed = new HashMap<>();
 
-    public static void pressButton(int button) {
+    public static void pressKey(int button) {
         keyboardButtonPressed.put(button, true);
     }
 
-    public static void releaseButton(int button) {
+    public static void releaseKey(int button) {
         keyboardButtonPressed.put(button, false);
     }
 
-    public static boolean isButtonPressed(int button) {
+    public static boolean isKeyPressed(int button) {
         return keyboardButtonPressed.getOrDefault(button, false);
     }
 }
