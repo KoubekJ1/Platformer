80d2dbfbe1caf90a7952e8d072c9956adda059ca
Jan Koubek <koubek.jenda@icloud.com>
2024-04-20 12:41:40 +0200

    implemented serialization to Block class

--- src/game/level/Block.java
+++ src/game/level/Block.java
@@ -3,12 +3,32 @@ package game.level;
 import renderer.Sprite;
 
 import java.awt.image.BufferedImage;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-public class Block {
+public class Block implements Serializable {
+    private static final String BLOCKS_DIRECTORY = "assets/blocks/";
+
+    private String blockID;
+    private String name;
+
     private Sprite sprite;
     private boolean collision;
 
     public BufferedImage getCurrentImage(float currentBlockSize) {
         return sprite.getCurrentImage(currentBlockSize);
+    }
+
+    public void serialize() throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(BLOCKS_DIRECTORY + blockID + ".block");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(this);
+
+        objectOutputStream.flush();
+        objectOutputStream.close();
+        fileOutputStream.flush();
+        fileOutputStream.close();
     }
 }
