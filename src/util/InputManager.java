fab9054f1b536526b554548bc50392a7c56053f6
Jan Koubek <koubek.jenda@icloud.com>
2024-04-20 16:45:40 +0200

    fixed methods not being static

--- src/util/InputManager.java
+++ src/util/InputManager.java
@@ -5,15 +5,15 @@ import java.util.HashMap;
 public abstract class InputManager {
     private static HashMap<Integer, Boolean> keyboardButtonPressed = new HashMap<>();
 
-    public void pressButton(int button) {
+    public static void pressButton(int button) {
         keyboardButtonPressed.put(button, true);
     }
 
-    public void releaseButton(int button) {
+    public static void releaseButton(int button) {
         keyboardButtonPressed.put(button, false);
     }
 
-    public boolean getButtonPressed(int button) {
+    public static boolean getButtonPressed(int button) {
         return keyboardButtonPressed.getOrDefault(button, false);
     }
 }
