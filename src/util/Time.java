aef04b6bafd7ce79d88840b132f5ec146a640c7f
Jan Koubek <koubek.jenda@icloud.com>
2024-04-20 13:25:38 +0200

    created Time class to keep track of time passed between frames

---
+++ src/util/Time.java
@@ -0,0 +1,9 @@
+package util;
+
+public class Time {
+    private static float timeStarted = System.nanoTime();
+
+    public static float getTime() {
+        return (float) ((System.nanoTime() - timeStarted) * 1E-9);
+    }
+}
