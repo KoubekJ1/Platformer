88a25bdc558c5abebba2f354470e2e3d7e98ecee
Jan Koubek <koubek.jenda@icloud.com>
2024-04-20 12:55:37 +0200

    created RenderInfo class and added its fields, constructor and getters and setters

---
+++ src/renderer/RenderInfo.java
@@ -0,0 +1,65 @@
+package renderer;
+
+import game.level.Block;
+import game.level.Camera;
+import game.level.Player;
+import game.level.enemy.Enemy;
+
+import java.awt.*;
+import java.util.LinkedList;
+
+public class RenderInfo {
+    private Color background;
+    private Camera camera;
+    private Block[][] blocks;
+    private LinkedList<Player> players;
+    private LinkedList<Enemy> enemies;
+
+    public RenderInfo(Color background, Camera camera, Block[][] blocks, LinkedList<Player> players, LinkedList<Enemy> enemies) {
+        this.background = background;
+        this.camera = camera;
+        this.blocks = blocks;
+        this.players = players;
+        this.enemies = enemies;
+    }
+
+    public Color getBackground() {
+        return background;
+    }
+
+    public void setBackground(Color background) {
+        this.background = background;
+    }
+
+    public Camera getCamera() {
+        return camera;
+    }
+
+    public void setCamera(Camera camera) {
+        this.camera = camera;
+    }
+
+    public Block[][] getBlocks() {
+        return blocks;
+    }
+
+    public void setBlocks(Block[][] blocks) {
+        this.blocks = blocks;
+    }
+
+    public LinkedList<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(LinkedList<Player> players) {
+        this.players = players;
+    }
+
+    public LinkedList<Enemy> getEnemies() {
+        return enemies;
+    }
+
+    public void setEnemies(LinkedList<Enemy> enemies) {
+        this.enemies = enemies;
+    }
+}
