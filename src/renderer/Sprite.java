bc4290eb43c9c063024f89efb84d7e7cd0b60a4f
Jan Koubek <koubek.jenda@icloud.com>
2024-04-21 20:17:07 +0200

    added getter for remaining time in animation

--- src/renderer/Sprite.java
+++ src/renderer/Sprite.java
@@ -111,4 +111,8 @@ public class Sprite {
     public void setHeight(float height) {
         this.height = height;
     }
+
+    public int getAnimationTimeLeft() {
+        return currentAnimation.getTimeLeft();
+    }
 }
