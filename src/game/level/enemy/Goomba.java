3b068daf1e52698cc77bd722571b9029e75adfa4
Jan Koubek <koubek.jenda@icloud.com>
2024-04-21 20:05:19 +0200

    created Goomba class

---
+++ src/game/level/enemy/Goomba.java
@@ -0,0 +1,17 @@
+package game.level.enemy;
+
+public class Goomba extends EnemyBehavior {
+    public Goomba(Enemy parentEnemy) {
+        super(parentEnemy);
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void damage() {
+
+    }
+}
