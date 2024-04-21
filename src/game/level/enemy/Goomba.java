d298a36db18f20b9c1edbed026f28ed35743cb6f
Jan Koubek <koubek.jenda@icloud.com>
2024-04-21 20:36:31 +0200

    implemented part of Goomba update() method

--- src/game/level/enemy/Goomba.java
+++ src/game/level/enemy/Goomba.java
@@ -1,17 +1,33 @@
 package game.level.enemy;
 
 public class Goomba extends EnemyBehavior {
-    public Goomba(Enemy parentEnemy) {
-        super(parentEnemy);
+    private static final float SPEED = 1;
+    private boolean direction;
+
+    public Goomba() {
+        this.direction = true;
+    }
+
+    public Goomba(boolean direction) {
+        this.direction = direction;
     }
 
     @Override
     public void update(float dt) {
-
+        if (parentEnemy.isDead()) return;
+        parentEnemy.setPosition(parentEnemy.getPosition()[0] + SPEED * dt * getDirection(), parentEnemy.getPosition()[1]);
     }
 
     @Override
     public void damage() {
+        parentEnemy.kill();
+    }
 
+    private int getDirection() {
+        if (direction) {
+            return 1;
+        } else {
+            return -1;
+        }
     }
 }
