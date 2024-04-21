0515f7cd299fab7755dcd0d48a12af3a439137de
Jan Koubek <koubek.jenda@icloud.com>
2024-04-21 15:06:21 +0200

    optimized transform setters

--- src/game/level/player/camera/Camera.java
+++ src/game/level/player/camera/Camera.java
@@ -26,12 +26,12 @@ public class Camera {
     }
 
     public void setPosition(double x, double y) {
-        transform = new AffineTransform(transform.getScaleX(), 0, 0, transform.getScaleY(), -x, -y);
+        transform.setTransform(transform.getScaleX(), 0, 0, transform.getScaleY(), -x, -y);
     }
 
     public void setWorldScale(double scale) {
         // Weird workaround, the scale() method doesn't save translation, translation is applied again after scaling
-        transform = new AffineTransform(scale, 0, 0, scale, transform.getTranslateX(), transform.getTranslateY());
+        transform.setTransform(scale, 0, 0, scale, transform.getTranslateX(), transform.getTranslateY());
     }
 
     public void update() {
