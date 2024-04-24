4b5f72def7bcaa835b9c7f38bcc34f107c5e3cca
Jan Koubek <koubek.jenda@icloud.com>
2024-04-24 17:12:27 +0200

    modified Enemy to work as a subclass of Character

--- src/game/level/character/enemy/Enemy.java
+++ src/game/level/character/enemy/Enemy.java
@@ -2,6 +2,7 @@ package game.level.character.enemy;
 
 import game.ProgramManager;
 import game.level.Block;
+import game.level.character.Character;
 import renderer.Animation;
 import renderer.Sprite;
 import util.InputManager;
@@ -12,18 +13,12 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Enemy {
+public class Enemy extends Character {
     private static final float TERMINAL_VELOCITY = 50;
     private static final float GRAVITY_ACCELERATION = 1;
 
-    private Sprite sprite;
     private EnemyBehavior ai;
     private Timer deathTimer;
-
-    private float posX;
-    private float posY;
-    private float xVelocity;
-    private float yVelocity;
 
     public Enemy(Sprite sprite, EnemyBehavior ai, int posX, int posY) {
         this.sprite = sprite;
@@ -31,22 +26,17 @@ public class Enemy {
         ai.setParentEnemy(this);
         this.posX = posX;
         this.posY = posY;
-        xVelocity = 0;
-        yVelocity = 0;
+        velocityX = 0;
+        velocityY = 0;
     }
 
     public void playAnimation(String animation) {
         sprite.playAnimation(animation);
     }
 
-    public void update(float dt) {
-        applyGravity(dt);
-        groundCollisionCheck();
-        ceilingCollisionCheck();
+    @Override
+    public void characterUpdate(float dt) {
         ai.update(dt);
-
-        posX += xVelocity;
-        posY += yVelocity;
     }
 
     public void damage() {
@@ -85,65 +75,6 @@ public class Enemy {
     public boolean isDead() {
         if (deathTimer == null) return false;
         return deathTimer.isRunning();
-    }
-
-    private void applyGravity(float dt) {
-        float gravityEffectiveness = 1;
-        if (yVelocity < 0 && !(InputManager.isKeyPressed(KeyEvent.VK_W) || InputManager.isKeyPressed(KeyEvent.VK_SPACE) || InputManager.isKeyPressed(KeyEvent.VK_UP))) {
-            gravityEffectiveness = 3f;
-        }
-        yVelocity += GRAVITY_ACCELERATION * dt * gravityEffectiveness;
-        if (yVelocity > TERMINAL_VELOCITY) {
-            yVelocity = TERMINAL_VELOCITY;
-        }
-    }
-
-    private void groundCollisionCheck() {
-        for (float i = 0; i < yVelocity + 1 && yVelocity > 0; i++) {
-            if (i >= yVelocity) {
-                i = yVelocity;
-            }
-            if (Math.ceil(posY + i) >= ProgramManager.getLevel().getLevelSizeY()) break;
-            Block block1 = ProgramManager.getLevel().getBlock((int) posX, (int) Math.ceil(posY + i));
-            Block block2 = ProgramManager.getLevel().getBlock((int) Math.ceil(posX), (int) Math.ceil(posY + i));
-            if ((block1 != null && block1.isCollision()) || (block2 != null && block2.isCollision())) {
-                yVelocity = 0;
-                posY = (float) Math.floor(posY + i);
-                break;
-            }
-        }
-    }
-
-    private void ceilingCollisionCheck() {
-        for (float i = 0; i > yVelocity - 1 && yVelocity < 0; i--) {
-            if (i <= yVelocity) {
-                i = yVelocity;
-            }
-            if (Math.floor(posY + i) <= 0) break;
-            Block block1 = ProgramManager.getLevel().getBlock((int) posX, (int) Math.floor(posY + i));
-            Block block2 = ProgramManager.getLevel().getBlock((int) Math.ceil(posX), (int) Math.floor(posY + i));
-            if ((block1 != null && block1.isCollision()) || (block2 != null && block2.isCollision())) {
-                yVelocity = 0;
-                posY = (float) Math.ceil(posY + i);
-                break;
-            }
-        }
-    }
-
-    public float getXVelocity() {
-        return xVelocity;
-    }
-
-    public void setXVelocity(float xVelocity) {
-        this.xVelocity = xVelocity;
-    }
-
-    public float getYVelocity() {
-        return yVelocity;
-    }
-
-    public void setYVelocity(float yVelocity) {
-        this.yVelocity = yVelocity;
     }
 
     public BufferedImage getActiveImage(float currentBlockSize) {
