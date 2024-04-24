e604dd5ef4f3efa6df09e36654fef48844c75509
Jan Koubek <koubek.jenda@icloud.com>
2024-04-24 16:40:51 +0200

    added variables to Character class

--- src/game/level/character/Character.java
+++ src/game/level/character/Character.java
@@ -1,4 +1,15 @@
 package game.level.character;
 
+import renderer.Sprite;
+
 public class Character {
+    public static final int GRAVITY_ACCELERATION = 1;
+    public static final String CHARACTER_TEXTURES_PATH = "assets/textures/characters/";
+
+    protected Sprite sprite;
+
+    protected int posX;
+    protected int posY;
+    protected int velocityX;
+    protected int velocityY;
 }
