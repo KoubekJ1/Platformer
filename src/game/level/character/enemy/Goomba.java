2d8f5c2b03af65d2fb92e53421067479c610d794
Jan Koubek <koubek.jenda@icloud.com>
2024-04-24 17:16:05 +0200

    fixed some oversights

--- src/game/level/character/enemy/Goomba.java
+++ src/game/level/character/enemy/Goomba.java
@@ -18,8 +18,12 @@ public class Goomba extends EnemyBehavior {
     @Override
     public void update(float dt) {
         if (parentEnemy.isDead()) return;
-        rightBlockCollisionCheck();
-        leftBlockCollisionCheck();
+        if (parentEnemy.rightBlockCollisionCheck()) {
+            switchDirection();
+        }
+        if (parentEnemy.leftBlockCollisionCheck()) {
+            switchDirection();
+        }
         parentEnemy.setVelocityX(SPEED * dt * getDirection());
         parentEnemy.playAnimation("move");
     }
@@ -39,35 +43,5 @@ public class Goomba extends EnemyBehavior {
 
     private void switchDirection() {
         direction = !direction;
-    }
-
-    private void rightBlockCollisionCheck() {
-        for (float i = 0; i < parentEnemy.getVelocityX() + 1 && parentEnemy.getVelocityX() > 0; i++) {
-            if (i >= parentEnemy.getVelocityX()) {
-                i = parentEnemy.getVelocityX();
-            }
-            if (Math.ceil(parentEnemy.getPosition()[0] + i) >= ProgramManager.getLevel().getLevelSizeX()) break;
-            Block block1 = ProgramManager.getLevel().getBlock((int) (Math.ceil(parentEnemy.getPosition()[0] + i)), (int) parentEnemy.getPosition()[1]);
-            Block block2 = ProgramManager.getLevel().getBlock((int) Math.ceil(parentEnemy.getPosition()[0] + i), (int) Math.ceil(parentEnemy.getPosition()[1]));
-            if ((block1 != null && block1.isCollision()) || (block2 != null && block2.isCollision())) {
-                switchDirection();
-                break;
-            }
-        }
-    }
-
-    private void leftBlockCollisionCheck() {
-        for (float i = 0; i > parentEnemy.getVelocityX() - 1 && parentEnemy.getVelocityX() < 0; i--) {
-            if (i <= parentEnemy.getVelocityX()) {
-                i = parentEnemy.getVelocityX();
-            }
-            if (Math.floor(parentEnemy.getPosition()[1] + i) <= 0) break;
-            Block block1 = ProgramManager.getLevel().getBlock((int) Math.floor(parentEnemy.getPosition()[0] + i), (int) parentEnemy.getPosition()[1]);
-            Block block2 = ProgramManager.getLevel().getBlock((int) Math.floor(parentEnemy.getPosition()[0] + i), (int) Math.ceil(parentEnemy.getPosition()[1]));
-            if ((block1 != null && block1.isCollision()) || (block2 != null && block2.isCollision())) {
-                switchDirection();
-                break;
-            }
-        }
     }
 }
