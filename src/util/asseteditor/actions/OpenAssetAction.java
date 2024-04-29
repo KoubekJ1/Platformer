b19f6399e6c8a8ac1d3b0635a75d9a80704569fe
Jan Koubek <koubek.jenda@icloud.com>
2024-04-29 18:39:27 +0200

    created actions for AssetEditor menu

---
+++ src/util/asseteditor/actions/OpenAssetAction.java
@@ -0,0 +1,11 @@
+package util.asseteditor.actions;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class OpenAssetAction extends AbstractAction {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
---
+++ src/util/asseteditor/actions/SaveAssetAction.java
@@ -0,0 +1,11 @@
+package util.asseteditor.actions;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class SaveAssetAction extends AbstractAction {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
