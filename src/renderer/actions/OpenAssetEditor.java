9a6bb7840f2619bae0f68681f6e4d7ec0062f859
Jan Koubek <koubek.jenda@icloud.com>
2024-04-29 18:12:37 +0200

    implemented OpenAssetEditor action functionality

--- src/renderer/actions/OpenAssetEditor.java
+++ src/renderer/actions/OpenAssetEditor.java
@@ -1,11 +1,18 @@
 package renderer.actions;
 
+import util.AssetEditor;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 public class OpenAssetEditor extends AbstractAction {
+
+    public OpenAssetEditor() {
+        super("Asset Editor");
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        AssetEditor.showAssetEditor();
     }
 }
