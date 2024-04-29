5e1e8270e10c10c99d54e2fa2394a90f3099ad02
Jan Koubek <koubek.jenda@icloud.com>
2024-04-29 17:55:10 +0200

    created OpenAssetEditor class

---
+++ src/renderer/actions/OpenAssetEditor.java
@@ -0,0 +1,11 @@
+package renderer.actions;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class OpenAssetEditor extends AbstractAction {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
