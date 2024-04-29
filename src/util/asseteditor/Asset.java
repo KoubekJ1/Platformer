d935225e1245296795e4140bc272c80085d3cb61
Jan Koubek <koubek.jenda@icloud.com>
2024-04-29 19:32:53 +0200

    implemented Asset functionality

--- src/util/asseteditor/Asset.java
+++ src/util/asseteditor/Asset.java
@@ -1,12 +1,35 @@
 package util.asseteditor;
 
 import game.level.Block;
+import game.level.character.Powerup;
 import game.level.character.enemy.Enemy;
+import renderer.Animation;
+import renderer.Sprite;
 
 public class Asset {
     private AssetType assetType;
 
-    private Block block;
-    private Enemy enemy;
-    private Power
+    private Object asset;
+
+    private Object getAsset() {
+        return asset;
+    }
+
+    public void setAsset(Object asset) {
+        this.asset = asset;
+    }
+
+    public AssetType getAssetType() {
+        return assetType;
+    }
+
+    public static Object getNewAsset(AssetType type) {
+        return switch (type) {
+            case BLOCK -> new Block();
+            case ENEMY -> new Enemy();
+            case POWERUP -> new Powerup();
+            case SPRITE -> new Sprite();
+            case ANIMATION -> new Animation();
+        };
+    }
 }
