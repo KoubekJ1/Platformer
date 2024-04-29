b020990f580bb6ead2944888e1b1bd7afdd06e9d
Jan Koubek <koubek.jenda@icloud.com>
2024-04-29 19:35:04 +0200

    started AssetEditorPanel implementation

--- src/util/asseteditor/AssetEditorPanel.java
+++ src/util/asseteditor/AssetEditorPanel.java
@@ -1,11 +1,23 @@
 package util.asseteditor;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 
 public class AssetEditorPanel extends JPanel {
 
-    public AssetEditorPanel() {
+    private Asset asset;
 
+    public AssetEditorPanel() {
+        this.setBorder(new EmptyBorder(10, 10, 10, 10));
+        this.setLayout(new GridLayout(2, 2, 10, 10));
+
+
+    }
+
+    public void setAsset(AssetType type) {
+        asset = new Asset();
+        asset.setAsset(Asset.getNewAsset(type));
     }
 
 }
