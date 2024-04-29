2055e767784a1dedb420d01f311917acfee29203
Jan Koubek <koubek.jenda@icloud.com>
2024-04-29 18:12:55 +0200

    declared methods, changed class structure

---
+++ src/util/AssetEditor.java
@@ -0,0 +1,27 @@
+package util;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AssetEditor {
+
+    private static AssetEditor editor;
+    private JFrame window;
+
+    public static void showAssetEditor() {
+        editor = new AssetEditor();
+    }
+
+    private AssetEditor() {
+
+    }
+
+    private void initializeWindow() {
+
+    }
+
+    private void showWindow() {
+
+    }
+
+}
