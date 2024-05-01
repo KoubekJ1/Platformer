65ff64473061966d7763890f112b9b49df4cf348
Jan Koubek <koubek.jenda@icloud.com>
2024-05-01 14:36:38 +0200

    declared Powerup methods

--- src/game/level/character/Powerup.java
+++ src/game/level/character/Powerup.java
@@ -1,4 +1,19 @@
 package game.level.character;
 
-public class Powerup {
+public class Powerup extends DynamicObject {
+
+    @Override
+    protected void objectUpdate(float dt) {
+
+    }
+
+    @Override
+    protected void damage() {
+        kill();
+    }
+
+    @Override
+    protected void kill() {
+        
+    }
 }
