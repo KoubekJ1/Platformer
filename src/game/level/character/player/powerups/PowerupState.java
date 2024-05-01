60532312a64d7f7b9e5f09aafb53891b87d5621b
Jan Koubek <koubek.jenda@icloud.com>
2024-05-01 16:03:37 +0200

    created PowerupState class

---
+++ src/game/level/character/player/powerups/PowerupState.java
@@ -0,0 +1,12 @@
+package game.level.character.player.powerups;
+
+import game.level.character.player.Player;
+
+public class PowerupState {
+
+    private Player parentPlayer;
+
+    public PowerupState(Player parentPlayer) {
+        this.parentPlayer = parentPlayer;
+    }
+}
