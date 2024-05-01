8aa3bdef42d25662a8220253608070f61648bb13
Jan Koubek <koubek.jenda@icloud.com>
2024-05-01 16:05:28 +0200

    added abstract methods to PowerupState class

--- src/game/level/character/player/powerups/PowerupState.java
+++ src/game/level/character/player/powerups/PowerupState.java
@@ -2,11 +2,15 @@ package game.level.character.player.powerups;
 
 import game.level.character.player.Player;
 
-public class PowerupState {
+public abstract class PowerupState {
 
     private Player parentPlayer;
 
     public PowerupState(Player parentPlayer) {
         this.parentPlayer = parentPlayer;
     }
+
+    public abstract void damage();
+
+    public abstract void getSprite();
 }
