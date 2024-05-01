5012295ccf16f4e40143b087d97a8414d8010a3b
Jan Koubek <koubek.jenda@icloud.com>
2024-05-01 15:14:23 +0200

    implemented power-up objectUpdate() method

--- src/game/level/character/Powerup.java
+++ src/game/level/character/Powerup.java
@@ -4,9 +4,36 @@ import game.ProgramManager;
 
 public class Powerup extends DynamicObject {
 
+    private static final float POWERUP_SPEED = 1;
+
+    private boolean direction;
+
+    public Powerup() {
+        this.direction = true;
+    }
+
     @Override
     protected void objectUpdate(float dt) {
+        if (rightBlockCollisionCheck()) {
+            switchDirection();
+        }
+        if (leftBlockCollisionCheck()) {
+            switchDirection();
+        }
 
+        velocityX = POWERUP_SPEED * getDirection() * dt;
+    }
+
+    private int getDirection() {
+        if (direction) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    private void switchDirection() {
+        direction = !direction;
     }
 
     @Override
