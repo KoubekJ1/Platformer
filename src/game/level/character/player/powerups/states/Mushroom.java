868e8ac87277394221625d3a0637445e868400e1
Jan Koubek <koubek.jenda@icloud.com>
2024-05-01 16:16:22 +0200

    created PowerupState subclasses

---
+++ src/game/level/character/player/powerups/states/Mushroom.java
@@ -0,0 +1,39 @@
+package game.level.character.player.powerups.states;
+
+import game.level.character.player.Player;
+import renderer.Animation;
+import renderer.Sprite;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Mushroom extends PowerupState {
+
+    public Mushroom(Player parentPlayer) {
+        super(parentPlayer);
+    }
+
+    @Override
+    public void damage() {
+        parentPlayer.setPowerupState(new Small(parentPlayer));
+    }
+
+    @Override
+    public Sprite getSprite() {
+        HashMap<String, Animation> animations = new HashMap<>();
+        ArrayList<String> images = new ArrayList<>();
+
+        images.add(PLAYER_TEXTURES_DIRECTORY + "large/run1.png");
+        images.add(PLAYER_TEXTURES_DIRECTORY + "large/run2.png");
+        images.add(PLAYER_TEXTURES_DIRECTORY + "large/run3.png");
+
+        animations.put("running", new Animation(images, 80, true));
+        animations.put("sprinting", new Animation(images, 40, true));
+        animations.put("static", new Animation(PLAYER_TEXTURES_DIRECTORY + "large/static.png"));
+        animations.put("air", new Animation(PLAYER_TEXTURES_DIRECTORY + "large/air.png"));
+        animations.put("turn", new Animation(PLAYER_TEXTURES_DIRECTORY + "large/turn.png"));
+        animations.put("kill", new Animation(PLAYER_TEXTURES_DIRECTORY + "large/kill.png"));
+
+        return new Sprite(animations, 1, 2);
+    }
+}
---
+++ src/game/level/character/player/powerups/states/Small.java
@@ -0,0 +1,38 @@
+package game.level.character.player.powerups.states;
+
+import game.level.character.player.Player;
+import renderer.Animation;
+import renderer.Sprite;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Small extends PowerupState {
+    public Small(Player parentPlayer) {
+        super(parentPlayer);
+    }
+
+    @Override
+    public void damage() {
+        parentPlayer.kill();
+    }
+
+    @Override
+    public Sprite getSprite() {
+        HashMap<String, Animation> animations = new HashMap<>();
+        ArrayList<String> images = new ArrayList<>();
+
+        images.add(PLAYER_TEXTURES_DIRECTORY + "small/run1.png");
+        images.add(PLAYER_TEXTURES_DIRECTORY + "small/run2.png");
+        images.add(PLAYER_TEXTURES_DIRECTORY + "small/run3.png");
+
+        animations.put("running", new Animation(images, 80, true));
+        animations.put("sprinting", new Animation(images, 40, true));
+        animations.put("static", new Animation(PLAYER_TEXTURES_DIRECTORY + "small/static.png"));
+        animations.put("air", new Animation(PLAYER_TEXTURES_DIRECTORY + "small/air.png"));
+        animations.put("turn", new Animation(PLAYER_TEXTURES_DIRECTORY + "small/turn.png"));
+        animations.put("kill", new Animation(PLAYER_TEXTURES_DIRECTORY + "small/kill.png"));
+
+        return new Sprite(animations, 1, 1);
+    }
+}
