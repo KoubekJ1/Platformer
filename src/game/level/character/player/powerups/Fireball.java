48994db08a0692ac4846bbe3ffbdfc56522a3531
Jan Koubek <koubek.jenda@icloud.com>
2024-05-03 20:57:10 +0200

    implemented Fireball functionality

--- src/game/level/character/player/powerups/Fireball.java
+++ src/game/level/character/player/powerups/Fireball.java
@@ -1,21 +1,31 @@
 package game.level.character.player.powerups;
 
+import game.ProgramManager;
 import game.level.character.DynamicObject;
 
 public class Fireball extends DynamicObject {
+
+    private static final float SPEED  = 5;
+    private static final float JUMP_VELOCITY  = 1;
+
     @Override
     protected void objectUpdate(float dt) {
+        if (velocityY == 0) jump();
+        velocityX = SPEED * dt;
+    }
 
+    private void jump() {
+        velocityY = JUMP_VELOCITY;
     }
 
     @Override
     protected void damage() {
-
+        kill();
     }
 
     @Override
     public void kill() {
-
+        ProgramManager.getLevel().removeObject(this);
     }
 
     @Override
