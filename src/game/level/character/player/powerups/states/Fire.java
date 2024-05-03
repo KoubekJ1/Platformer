a48e1b7b7d197e5649860431b4393c2081693e11
Jan Koubek <koubek.jenda@icloud.com>
2024-05-03 20:47:31 +0200

    created Fire class

---
+++ src/game/level/character/player/powerups/states/Fire.java
@@ -0,0 +1,26 @@
+package game.level.character.player.powerups.states;
+
+import game.level.character.player.Player;
+import renderer.Sprite;
+
+public class Fire extends PowerupState {
+
+    public Fire(Player parentPlayer) {
+        super(parentPlayer);
+    }
+
+    @Override
+    public void damage() {
+
+    }
+
+    @Override
+    public void ability() {
+
+    }
+
+    @Override
+    public Sprite getSprite() {
+        return null;
+    }
+}
