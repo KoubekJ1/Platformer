e68e208b2edcabffb294ea59de8cbae6ec01bfed
Jan Koubek <koubek.jenda@icloud.com>
2024-05-03 20:53:20 +0200

    created Fireball class

---
+++ src/game/level/character/player/powerups/Fireball.java
@@ -0,0 +1,30 @@
+package game.level.character.player.powerups;
+
+import game.level.character.DynamicObject;
+
+public class Fireball extends DynamicObject {
+    @Override
+    protected void objectUpdate(float dt) {
+
+    }
+
+    @Override
+    protected void damage() {
+
+    }
+
+    @Override
+    public void kill() {
+
+    }
+
+    @Override
+    protected String getAssetDirectory() {
+        return null;
+    }
+
+    @Override
+    protected String getAssetExtension() {
+        return null;
+    }
+}
