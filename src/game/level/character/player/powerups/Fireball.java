d3ab9daf12d8f5bd21a6a20a37e20aa3b69db4b9
Jan Koubek <koubek.jenda@icloud.com>
2024-05-03 21:08:37 +0200

    finished Fireball implementation

--- src/game/level/character/player/powerups/Fireball.java
+++ src/game/level/character/player/powerups/Fireball.java
@@ -2,16 +2,45 @@ package game.level.character.player.powerups;
 
 import game.ProgramManager;
 import game.level.character.DynamicObject;
+import renderer.Animation;
+import renderer.Sprite;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Fireball extends DynamicObject {
 
     private static final float SPEED  = 5;
     private static final float JUMP_VELOCITY  = 1;
 
+    public Fireball(float x, float y) {
+        this.setPosX(x);
+        this.setPosY(y);
+
+        // Just in case there's somehow no animation playing to prevent errors
+        Animation staticAnimation = new Animation("projectiles/fireball/1.png");
+
+        ArrayList<String> moveImages = new ArrayList<>();
+        moveImages.add("projectiles/fireball/1.png");
+        moveImages.add("projectiles/fireball/2.png");
+        moveImages.add("projectiles/fireball/3.png");
+        moveImages.add("projectiles/fireball/4.png");
+        Animation move = new Animation(moveImages, 50, true);
+
+        HashMap<String, Animation> animations = new HashMap<>();
+        animations.put("static", staticAnimation);
+        animations.put("move", move);
+
+        sprite = new Sprite(animations, 1, 1);
+        sprite.playAnimation("move");
+    }
+
     @Override
     protected void objectUpdate(float dt) {
         if (velocityY == 0) jump();
         velocityX = SPEED * dt;
+        if (leftBlockCollisionCheck()) kill();
+        if (rightBlockCollisionCheck()) kill();
     }
 
     private void jump() {
