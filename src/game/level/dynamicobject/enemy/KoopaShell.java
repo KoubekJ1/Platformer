02c90bb5e5dfa508f0bffd5fe1b6247554b33a87
Jan Koubek <koubek.jenda@icloud.com>
2024-05-04 14:17:46 +0200

    fixed bugs

--- src/game/level/dynamicobject/enemy/KoopaShell.java
+++ src/game/level/dynamicobject/enemy/KoopaShell.java
@@ -1,13 +1,51 @@
 package game.level.dynamicobject.enemy;
 
 public class KoopaShell extends EnemyBehavior {
+
+    private static final float SPEED = 5;
+    private boolean direction;
+    private boolean moving;
+
+    public KoopaShell() {
+        parentEnemy.setSizeY(1);
+        this.direction = true;
+        this.moving = false;
+    }
+
+    public KoopaShell(boolean direction) {
+        parentEnemy.setSizeY(1);
+        this.direction = direction;
+        this.moving = false;
+    }
+
     @Override
     public void update(float dt) {
-
+        if (!moving) return;
+        if (parentEnemy.isDead()) return;
+        if (parentEnemy.rightBlockCollisionCheck()) {
+            switchDirection();
+        }
+        if (parentEnemy.leftBlockCollisionCheck()) {
+            switchDirection();
+        }
+        parentEnemy.setVelocityX(SPEED * dt * getDirection());
+        parentEnemy.playAnimation("move");
     }
 
     @Override
     public void damage() {
+        this.moving = !moving;
+    }
 
+    private int getDirection() {
+        if (direction) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    private void switchDirection() {
+        direction = !direction;
     }
 }
