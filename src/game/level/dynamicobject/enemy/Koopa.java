d41b50063eea26f850d71f0640e83db92eda8a2e
Jan Koubek <koubek.jenda@icloud.com>
2024-05-04 13:59:48 +0200

    created Koopa class

---
+++ src/game/level/dynamicobject/enemy/Koopa.java
@@ -0,0 +1,13 @@
+package game.level.dynamicobject.enemy;
+
+public class Koopa extends EnemyBehavior {
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void damage() {
+
+    }
+}
