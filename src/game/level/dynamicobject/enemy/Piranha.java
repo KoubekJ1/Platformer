ba9f688ef38f68a5d2dbb818fceb7e44349076a4
Jan Koubek <koubek.jenda@icloud.com>
2024-05-10 12:54:11 +0200

    added factory method for piranha plant

--- src/game/level/dynamicobject/enemy/Piranha.java
+++ src/game/level/dynamicobject/enemy/Piranha.java
@@ -1,5 +1,12 @@
 package game.level.dynamicobject.enemy;
 
+import renderer.Animation;
+import renderer.Sprite;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Piranha extends EnemyBehavior {
     @Override
     public void update(float dt) {
@@ -9,5 +16,24 @@ public class Piranha extends EnemyBehavior {
     @Override
     public void damage() {
 
+    }
+
+    public static Enemy getPiranha(float x, float y) {
+        HashMap<String, Animation> animations = new HashMap<>();
+        ArrayList<String> piranhaImages = new ArrayList<>();
+        piranhaImages.add("characters/enemies/piranha/1.png");
+        piranhaImages.add("characters/enemies/piranha/2.png");
+        Animation piranhaAnimation = new Animation(piranhaImages, 50, true);
+
+        Enemy piranha = new Enemy("Piranha", "piranha", new Sprite(animations, 1, 2), new Piranha());
+        try {
+            piranha.serialize();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        piranha.setPosX(x);
+        piranha.setPosY(y);
+
+        return piranha;
     }
 }
