a6df0814e022ec6c3612a1b9e12ee37b0586fd8f
Jan Koubek <koubek.jenda@icloud.com>
2024-05-11 13:36:50 +0200

    implemented Score functionality

--- src/game/level/Score.java
+++ src/game/level/Score.java
@@ -1,4 +1,43 @@
 package game.level;
 
+import game.level.dynamicobject.player.Player;
+
+import java.util.HashMap;
+
 public class Score {
+    private static final int[] MULTIPLIER_PROGRESSION = new int[]{1, 2, 4, 5, 8, 10, 20, 40, 50, 80};
+
+    private long score;
+
+    private HashMap<Player, Integer> multiplier;
+
+    public Score() {
+        this.score = 0;
+        this.multiplier = new HashMap<>();
+    }
+
+    public void increaseMultiplier(Player player) {
+        if (!multiplier.containsKey(player)) {
+            multiplier.put(player, 0);
+            return;
+        }
+        int nextMultiplier = multiplier.get(player) + 1;
+        if (nextMultiplier >= MULTIPLIER_PROGRESSION.length) return;
+        multiplier.put(player, nextMultiplier);
+    }
+
+    public int getMultiplier(Player player) {
+        if (!multiplier.containsKey(player)) {
+            multiplier.put(player, 0);
+        }
+        return MULTIPLIER_PROGRESSION[multiplier.get(player)];
+    }
+
+    public void addScore(int score) {
+        this.score += score;
+    }
+
+    public long getScore() {
+        return score;
+    }
 }
