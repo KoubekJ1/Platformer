aaa706e394b39929addb18c7e70f9d43f061bd67
Jan Koubek <koubek.jenda@icloud.com>
2024-05-12 14:37:59 +0200

    modified score to be able to keep track of dynamicObject multipliers as a whole instead of just players

--- src/game/level/Score.java
+++ src/game/level/Score.java
@@ -1,41 +1,42 @@
 package game.level;
 
-import game.level.dynamicobject.player.Player;
+import game.level.dynamicobject.DynamicObject;
 
 import java.util.HashMap;
 
 public class Score {
     public static final int ENEMY_DAMAGE_SCORE = 100;
+    public static final int KOOPA_DAMAGE_SCORE = 100;
 
     private static final int[] MULTIPLIER_PROGRESSION = new int[]{1, 2, 4, 5, 8, 10, 20, 40, 50, 80};
 
     private long score;
 
-    private HashMap<Player, Integer> multiplier;
+    private HashMap<DynamicObject, Integer> multiplier;
 
     public Score() {
         this.score = 0;
         this.multiplier = new HashMap<>();
     }
 
-    public void increaseMultiplier(Player player) {
-        if (!multiplier.containsKey(player)) {
-            multiplier.put(player, 0);
+    public void increaseMultiplier(DynamicObject dynamicObject) {
+        if (!multiplier.containsKey(dynamicObject)) {
+            multiplier.put(dynamicObject, 0);
         }
-        int nextMultiplier = multiplier.get(player) + 1;
+        int nextMultiplier = multiplier.get(dynamicObject) + 1;
         if (nextMultiplier >= MULTIPLIER_PROGRESSION.length) return;
-        multiplier.put(player, nextMultiplier);
+        multiplier.put(dynamicObject, nextMultiplier);
     }
 
-    public void resetMultiplier(Player player) {
-        multiplier.put(player, 0);
+    public void resetMultiplier(DynamicObject dynamicObject) {
+        multiplier.put(dynamicObject, 0);
     }
 
-    public int getMultiplier(Player player) {
-        if (!multiplier.containsKey(player)) {
-            multiplier.put(player, 0);
+    public int getMultiplier(DynamicObject dynamicObject) {
+        if (!multiplier.containsKey(dynamicObject)) {
+            multiplier.put(dynamicObject, 0);
         }
-        return MULTIPLIER_PROGRESSION[multiplier.get(player)];
+        return MULTIPLIER_PROGRESSION[multiplier.get(dynamicObject)];
     }
 
     public void addScore(int score) {
