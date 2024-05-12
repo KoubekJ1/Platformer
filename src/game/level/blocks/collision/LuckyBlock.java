19e11ceaa08caa14159b045bcce014bcfde3dedd
Jan Koubek <koubek.jenda@icloud.com>
2024-05-12 14:18:06 +0200

    added factory method for a lucky block

--- src/game/level/blocks/collision/LuckyBlock.java
+++ src/game/level/blocks/collision/LuckyBlock.java
@@ -1,7 +1,13 @@
 package game.level.blocks.collision;
 
 import game.ProgramManager;
+import game.level.blocks.Block;
 import game.level.dynamicobject.DynamicObject;
+import renderer.Animation;
+import renderer.Sprite;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class LuckyBlock implements Collidable {
 
@@ -16,5 +22,20 @@ public class LuckyBlock implements Collidable {
         content.setPosX(blockX);
         content.setPosX(blockY - 1);
         ProgramManager.getLevel().addObject(content);
+    }
+
+    public Block getLuckyBlock(DynamicObject content) {
+        HashMap<String, Animation> animations = new HashMap<>();
+
+        ArrayList<String> animationImages = new ArrayList<>();
+        animationImages.add("blocks/luckyblock/1.png");
+        animationImages.add("blocks/luckyblock/2.png");
+        animationImages.add("blocks/luckyblock/3.png");
+
+        animations.put("static", new Animation(animationImages, 50, true));
+
+        Sprite sprite = new Sprite(animations, 1, 1);
+        Block luckyBlock = new Block(sprite, true, new LuckyBlock(content));
+        return luckyBlock;
     }
 }
