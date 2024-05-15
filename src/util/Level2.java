1770df825a5719719db1b326f9d1d77af07bc7e3
Jan Koubek <koubek.jenda@icloud.com>
2024-05-15 17:21:07 +0200

    made more progress on the second level

--- src/util/Level2.java
+++ src/util/Level2.java
@@ -2,20 +2,38 @@ package util;
 
 import game.level.Level;
 import game.level.blocks.Block;
+import game.level.dynamicobject.enemy.Goomba;
 
 public class Level2 {
     public static Level getLevel() {
-        Level level = new Level("2", "2", 200, 30, Level.SKY_COLOR, 20, 19);
+        Level level = new Level("2", "2", 45, 100, Level.SKY_COLOR, 4, 89);
 
-        for (int i = 0; i < 200; i++) {
-            for (int j = 20; j < 30; j++) {
-                level.addBlock(Block.getGround(), i, j);
+        int islandWidth = 20;
+        for (int i = 0; i < level.getLevelSizeX(); i += islandWidth + 5) {
+            for (int k = 0; k < islandWidth; k++) {
+                for (int j = 90; j < 90 + (islandWidth/2)- Math.abs(k - Math.ceil((double) islandWidth / 2)); j++) {
+                    level.addBlock(Block.getGround(), i + k, j);
+                }
             }
         }
 
-        for (int i = 0; i < 20; i++) {
-            level.addBlock(new Block("void.png", true), 16, i);
+        for (int i = 0; i < level.getLevelSizeX(); i += islandWidth + 5) {
+            int counter = 0;
+            for (int j = 89; j > 20; j--) {
+                Block block;
+                if (counter >= 3) {
+                    block = Block.getUnbreakableBlock();
+                    counter = 0;
+                } else {
+                    block = Block.getBrick();
+                    counter++;
+                }
+                level.addBlock(block, i + 2, j);
+                level.addBlock(block, i + islandWidth - 2, j);
+            }
         }
+
+        level.addObject(Goomba.getGoomba(13, 89));
 
         return level;
     }
