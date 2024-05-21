1c910810fd9a963d1e2a4d8490744c14e8e5dabc
Jan Koubek <koubek.jenda@icloud.com>
2024-05-21 06:11:15 +0200

    created Finish class

---
+++ src/game/level/dynamicobject/Finish.java
@@ -0,0 +1,38 @@
+package game.level.dynamicobject;
+
+public class Finish extends DynamicObject {
+
+    public Finish() {
+
+    }
+
+    @Override
+    protected void objectUpdate(float dt) {
+
+    }
+
+    @Override
+    protected void damage() {
+
+    }
+
+    @Override
+    public void kill() {
+
+    }
+
+    @Override
+    public String getObjectCategory() {
+        return null;
+    }
+
+    @Override
+    protected String getAssetDirectory() {
+        return null;
+    }
+
+    @Override
+    protected String getAssetExtension() {
+        return null;
+    }
+}
