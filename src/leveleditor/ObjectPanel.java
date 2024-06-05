5fb3128097e6c7e46ba8cc6a65a68fc71b1c918a
Jan Koubek <koubek.jenda@icloud.com>
2024-06-05 19:27:57 +0200

    added stuff

--- src/leveleditor/ObjectPanel.java
+++ src/leveleditor/ObjectPanel.java
@@ -1,12 +1,20 @@
 package leveleditor;
 
+import game.level.blocks.Block;
+import game.level.dynamicobject.DynamicObject;
+import leveleditor.actions.SelectBlock;
 import leveleditor.actions.SwitchObjectType;
 import util.AssetManager;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class ObjectPanel extends JPanel {
+
+    private Block selectedBlock;
+    private DynamicObject selectedObject;
 
     private JPanel objects;
     private CardLayout objectSelectorCardLayout;
@@ -25,12 +33,15 @@ public class ObjectPanel extends JPanel {
         objects.add((blocks = new JPanel()), "blocks");
         objects.add((dynamicObjects = new JPanel()), "dynamicObjects");
 
+        blocks.setLayout(new FlowLayout());
+        dynamicObjects.setLayout(new FlowLayout());
+
         this.add(objects, BorderLayout.CENTER);
 
         objectTypeSelector = new JToolBar("Object type");
         objectTypeSelector.setFloatable(false);
-        objectTypeSelector.add(new JButton(new SwitchObjectType("Blocks", new ImageIcon(AssetManager.getTexture("blocks/brick.png")), "blocks", this)));
-        objectTypeSelector.add(new JButton(new SwitchObjectType("Dynamic Objects", new ImageIcon(AssetManager.getTexture("characters/player/small/air.png")), "dynamicObjects", this)));
+        objectTypeSelector.add(new JButton(new SwitchObjectType(new ImageIcon(AssetManager.getTexture("blocks/brick.png")), "blocks", this)));
+        objectTypeSelector.add(new JButton(new SwitchObjectType(new ImageIcon(AssetManager.getTexture("characters/player/small/air.png")), "dynamicObjects", this)));
 
         this.add(objectTypeSelector, BorderLayout.NORTH);
 
@@ -39,5 +50,56 @@ public class ObjectPanel extends JPanel {
 
     public void switchCard(String card) {
         objectSelectorCardLayout.show(objects, card);
+    }
+
+    private void loadObjects() {
+        loadBlocks();
+        loadDynamicObjects();
+    }
+
+    private void loadBlocks() {
+        File[] files = new File("assets/blocks").listFiles();
+
+        for (File file : files) {
+            try {
+                Block block = AssetManager.getBlock(file.getPath());
+                JButton button = new JButton(new SelectBlock(block, this));
+                blocks.add(button);
+            } catch (IOException e) {
+            } catch (ClassNotFoundException e) {
+            }
+        }
+    }
+
+    private void loadDynamicObjects() {
+        File[] files = new File("assets/blocks").listFiles();
+
+        for (File file : files) {
+            try {
+                Block block = AssetManager.getBlock(file.getPath());
+                JButton button = new JButton(new SelectBlock(block, this));
+                blocks.add(button);
+            } catch (IOException e) {
+            } catch (ClassNotFoundException e) {
+            }
+        }
+    }
+
+    public Block getSelectedBlock() {
+        return selectedBlock;
+    }
+
+    public void setSelectedBlock(Block selectedBlock) {
+        this.selectedBlock = selectedBlock;
+        this.selectedBlock = null;
+    }
+
+    public DynamicObject getSelectedObject() {
+        return selectedObject;
+    }
+
+    public void setSelectedObject(DynamicObject selectedObject) {
+        this.selectedObject = selectedObject;
+        this.selectedBlock = null;
     }
 }
